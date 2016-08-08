1d41279cc84d97c40212a45fcceeced521d67e4d
Nikolay Digaev <ffs.cmp@gmail.com>
2016-08-08 16:41:08 +0500

    Test User validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,6 +13,14 @@ RSpec.describe User, type: :model do
     it { should have_db_index :email }
   end
 
+  describe 'validations' do
+    subject { build :user }
+
+    it { should validate_presence_of :email }
+    it { should validate_uniqueness_of :email }
+    it { should validate_length_of(:password).is_at_least(6) }
+  end
+
   describe '#token' do
     it 'encodes ID into token' do
       user = create :user
