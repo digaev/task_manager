59674edbc97fc177ab4370d51a9ad510c5475d59
Nikolay Digaev <ffs.cmp@gmail.com>
2016-08-09 19:41:30 +0500

    Add #destroy_user_session! to UserSession

--- app/controllers/concerns/user_session.rb
+++ app/controllers/concerns/user_session.rb
@@ -2,4 +2,8 @@ module UserSession
   def create_user_session!(user)
     session[:user_token] = user.token
   end
+
+  def destroy_user_session!
+    session.delete(:user_token)
+  end
 end
--- spec/controllers/web/concerns/user_session_spec.rb
+++ spec/controllers/web/concerns/user_session_spec.rb
@@ -3,11 +3,14 @@ RSpec.describe UserSession, type: :controller do
     include UserSession
   end
 
-  describe '#create_user_session!' do
-    it 'creates user session' do
+  describe '#create_user_session! and #destroy_user_session!' do
+    it 'creates and destroys user session' do
       user = create :user
       controller.create_user_session!(user)
       expect(session[:user_token]).to eq(user.token)
+
+      controller.destroy_user_session!
+      expect(session[:user_token]).to be(nil)
     end
   end
 end
