9f06e1ef03b27dbe944d4fa8f4ccf5570617f72d
Nikolay Digaev <ffs.cmp@gmail.com>
2016-08-09 17:33:30 +0500

    Implement and test Web::Sessions controller

--- app/controllers/web/sessions_controller.rb
+++ app/controllers/web/sessions_controller.rb
@@ -1,2 +1,17 @@
-class Web::SessionsController < ApplicationController
+class Web::SessionsController < Web::ApplicationController
+  def new
+    @form = Forms::Session.new
+  end
+
+  def create
+    @form = Forms::Session.new(params.require(:session).permit(:email, :password))
+    if @form.valid?
+      user = User.find_by_email(@form.email)
+      if user && user.authenticate(@form.password)
+        create_user_session!(user)
+        redirect_to user_tasks_path(user) and return
+      end
+    end
+    render :new
+  end
 end
--- spec/controllers/web/sessions_controller_spec.rb
+++ spec/controllers/web/sessions_controller_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe Web::SessionsController, type: :controller do
+  describe '#new' do
+    it 'creates instance of Forms::Session' do
+      expect(Forms::Session).to receive(:new)
+      get :new
+    end
+  end
 
+  describe '#create' do
+    it 'creates session with permitted params' do
+      user = create :user
+      form = Forms::Session.new(
+        email: user.email,
+        password: user.password
+      )
+      params = {
+        email: user.email,
+        password: user.password
+      }
+      permitted_params = ActionController::Parameters.new(params).permit(
+        :email, :password
+      )
+      expect(Forms::Session).to receive(:new).with(permitted_params).and_return(form)
+      post :create, params: { session: params }
+    end
+
+    context 'when create succeeds' do
+      before do
+        @user = create :user
+        allow(User).to receive(:create).and_return @user
+      end
+
+      it 'creates session and redirects to web/users/tasks#index' do
+        expect(controller).to receive(:create_user_session!).with(@user)
+        expect(User).to receive(:find_by_email).with(@user.email).and_return(@user)
+        expect(@user).to receive(:authenticate).with(@user.password).and_return(@user)
+
+        post :create, params: { session: { email: @user.email, password: @user.password } }
+        expect(response).to redirect_to(user_tasks_path(@user))
+      end
+    end
+
+    context 'when create failed' do
+      before do
+        @user = create :user
+        @user.password = 'invalid'
+        allow(User).to receive(:create).and_return @user
+      end
+
+      it 'renders #new' do
+        post :create, params: { session: { email: @user.email, password: @user.password } }
+        expect(response).to render_template(:new)
+      end
+    end
+  end
 end
