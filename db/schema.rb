8d7d1c3db74b94dcd469aa7c81d0e97612f4fe45
Nikolay Digaev <ffs.cmp@gmail.com>
2016-08-10 11:48:08 +0500

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808113148) do
+ActiveRecord::Schema.define(version: 20160810062708) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name",        null: false
+    t.string   "description"
+    t.string   "state"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                           null: false
