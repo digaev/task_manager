4564fa482401dec1f3c3d73982e704b33117e5e6
Nikolay Digaev <ffs.cmp@gmail.com>
2016-08-10 18:36:03 +0500

    [Web/Application] rescue_from ActiveRecord::RecordNotFound

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -1,3 +1,7 @@
 class Web::ApplicationController < ApplicationController
   include UserSession
+
+  rescue_from ActiveRecord::RecordNotFound do
+    render status: 404, plain: 'Not found.'
+  end
 end
