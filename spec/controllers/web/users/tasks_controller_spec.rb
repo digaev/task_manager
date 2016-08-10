d8bbca18655ca1c436d4fc1f787ad3f6b548924e
Nikolay Digaev <ffs.cmp@gmail.com>
2016-08-10 15:39:01 +0500

    [Web/Users] Test Tasks#create controller

--- spec/controllers/web/users/tasks_controller_spec.rb
+++ spec/controllers/web/users/tasks_controller_spec.rb
@@ -36,7 +36,31 @@ RSpec.describe Web::Users::TasksController, type: :controller do
       )
       expect(Task).to receive(:new).with(permitted_params).and_return(task)
       post :create, params: { user_id: task.user_id, task: params }
-      expect(@user.tasks.count).to eq(1)
+    end
+
+    context 'when create succeeds' do
+      before do
+        @task = build :task, user: @user
+        allow(@task).to receive(:save)
+      end
+
+      it 'creates task and redirects to web/user/tasks#index' do
+        post :create, params: { user_id: @task.user_id, task: {
+          name: @task.name, description: @task.description
+        } }
+        expect(@user.tasks.count).to eq(1)
+        expect(response).to redirect_to(user_tasks_path(@task.user))
+      end
+    end
+
+    context 'when create fails' do
+      it 'renders #new' do
+        post :create, params: { user_id: @user.id, task: {
+          name: '', description: ''
+        } }
+        expect(response.status).to eq(200)
+        expect(response).to render_template(:new)
+      end
     end
   end
 end
