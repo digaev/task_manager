825428fe4c4a095fc05d5f0231c6977a10688bf0
Nikolay Digaev <ffs.cmp@gmail.com>
2016-08-10 15:25:54 +0500

    [Web] Implement and test Task controller

--- app/controllers/web/users/tasks_controller.rb
+++ app/controllers/web/users/tasks_controller.rb
@@ -1,2 +1,21 @@
 class Web::Users::TasksController < Web::ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @tasks = Task.includes(:user)
+      .where(user_id: user_id_from_token)
+      .most_recent_first
+  end
+
+  def create
+    @task = Task.new(params.require(:task).permit(
+      :name, :description
+    ))
+    @task.user_id = user_id_from_token
+    if @task.save
+      redirect_to user_tasks_path(@task.user)
+    else
+      render :new
+    end
+  end
 end
--- spec/controllers/web/users/tasks_controller_spec.rb
+++ spec/controllers/web/users/tasks_controller_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Web::Users::TasksController, type: :controller do
+  describe '#index' do
+    before do
+      @user = create :user
+      controller.create_user_session!(@user)
+      allow(controller).to receive(:authenticate_user!)
+    end
 
+    it 'returns all tasks of user' do
+      tasks = Task.includes(:user).where(user_id: @user.id)
+
+      allow(Task).to receive(:includes).and_return(tasks)
+      expect(tasks).to receive(:where).with(user_id: @user.id).and_return(tasks)
+
+      get :index, params: { user_id: @user.id }
+    end
+  end
+
+  describe '#create' do
+    before do
+      @user = create :user
+      controller.create_user_session!(@user)
+      allow(controller).to receive(:authenticate_user!)
+    end
+
+    it 'creates task with permitted params' do
+      task = build :task, user: @user
+      params = {
+        name: task.name,
+        description: task.description
+      }
+      permitted_params = ActionController::Parameters.new(params).permit(
+        :name, :description
+      )
+      expect(Task).to receive(:new).with(permitted_params).and_return(task)
+      post :create, params: { user_id: task.user_id, task: params }
+      expect(@user.tasks.count).to eq(1)
+    end
+  end
 end
