3583ce87bac10025ddb8094d94ccbb9b722ced9f
Nikolay Digaev <ffs.cmp@gmail.com>
2016-08-11 19:44:14 +0500

    [Web] Add #nav_link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def nav_link(title, path, link_options = {})
+    link_options = {
+      title: title
+    }.merge!(link_options)
+
+    content_tag(:li, class: (:active if current_page?(path))) do
+      link_to title, path, link_options
+    end
+  end
 end
