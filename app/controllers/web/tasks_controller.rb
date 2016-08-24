fa0ecc50e1ab9e8e7ef96b739af9a54f06360c07
Nikolay Digaev <ffs.cmp@gmail.com>
2016-08-24 15:47:56 +0500

    Remove #change_status

--- app/controllers/web/tasks_controller.rb
+++ app/controllers/web/tasks_controller.rb
@@ -1,15 +1,13 @@
 class Web::TasksController < Web::ApplicationController
   before_action :authenticate_user!, except: [:index]
-  before_action :set_task, only: [:show, :edit, :update, :destroy, :change_state]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.includes(:user).order(created_at: :desc)
   end
 
   def create
-    @task = Task.new(params.require(:task).permit(
-      :name, :description, :state, :attachment
-    ))
+    @task = Task.new(task_params)
     @task.user_id = user_id_from_token
     if @task.save
       redirect_to user_tasks_path(@task.user)
@@ -19,26 +17,28 @@ class Web::TasksController < Web::ApplicationController
   end
 
   def update
-    if @task.update_attributes(params.require(:task).permit(
-      :name, :description, :state, :attachment
-    ))
-      redirect_to user_tasks_path(@task.user)
-    else
-      render :edit
+    success = @task.update_attributes(task_params)
+    respond_to do |format|
+      format.js do
+        if success
+          render js: 'document.location.reload();'
+        else
+          render js: "alert(\"Whoops!\\n\\n#{ @task.errors.full_messages.join('\n') }\");"
+        end
+      end
+      format.html do
+        if success
+          redirect_to user_tasks_path(@task.user)
+        else
+          render :edit
+        end
+      end
     end
   end
 
   def destroy
     @task.destroy
     redirect_to (request.referer || tasks_path)
-  end
-
-  def change_state
-    @task.update_attributes(state: params[:state])
-    respond_to do |format|
-      format.html { redirect_to action: :index }
-      format.js { render js: 'document.location.reload();' }
-    end
   end
 
   private
@@ -50,5 +50,11 @@ class Web::TasksController < Web::ApplicationController
       else
         Task.find_by_id_and_user_id!(params[:id], current_user.id)
       end
+  end
+
+  def task_params
+    params.require(:task).permit(
+      :name, :description, :state, :attachment
+    )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,7 @@ Rails.application.routes.draw do
       end
     end
 
-    resources :tasks do
-      match '/change_state/:state',
-        action: :change_state,
-        as: :change_state,
-        via: [:patch, :put],
-        on: :member
-    end
+    resources :tasks
 
     resources :sessions, only: [:new, :create] do
       delete :destroy, on: :collection
